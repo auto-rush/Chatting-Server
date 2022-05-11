65fbe7875d2754fb1df9b2ee531f8fc6c8b1f2be
Kim-SuBin <sb2504@naver.com>
2022-05-11 23:11:02 +0900

    [Init] 🎉 spring boot project

---
+++ src/main/java/com/autorush/rushchat/RushchatApplication.java
@@ -0,0 +1,13 @@
+package com.autorush.rushchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autorush/rushchat/RushchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autorush.rushchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
