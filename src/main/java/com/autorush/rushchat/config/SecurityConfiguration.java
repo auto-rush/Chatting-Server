bd5fc64117fd4308976c8bd13bd2037263c48c1a
Cherrue <th885172@gmail.com>
2022-06-22 01:23:35 +0900

    [✨Feat] 홈페이지 및 github login 기능 추가
    추가된 기능
    - Welcome Page(index.html), home.html 추가
    - github 로그인 추가
    - 로그인 성공 시 home 으로 forward 및 로그인 한 사용자 이름 출력
    - 보안 정책 추가(미인증 사용자 접속 가능 url, 에러 핸들링, csrf 설정)

---
+++ src/main/java/com/autorush/rushchat/config/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.autorush.rushchat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/", "/error", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .httpBasic(withDefaults())
+                .oauth2Login();
+        // @formatter:on
+        return http.build();
+    }
+
+}
