ef1a7ef98c4835a031a466130fbef9b20c92db13
Kim-SuBin <sb2504@naver.com>
2022-06-30 01:14:34 +0900

    [Feat] ✨ ApiResponse 추가
    ApiResponse :
    public ApiResponse<반환할타입> 메소드명 () {
        return ApiResponse.success(반환할값);
    }

---
+++ src/main/java/com/autorush/rushchat/common/response/ApiResponse.java
@@ -0,0 +1,27 @@
+package com.autorush.rushchat.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ApiResponse<T> {
+    private static final String SUCCESS_CODE = "0";
+    private static final String SUCCESS_MESSAGE = "success";
+
+    private String code;
+
+    private String message;
+
+    private T data;
+
+    public static ApiResponse<EmptyJsonResponse> success() {
+        return new ApiResponse<>(SUCCESS_CODE, SUCCESS_MESSAGE, new EmptyJsonResponse());
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(SUCCESS_CODE, SUCCESS_MESSAGE, data);
+    }
+}
