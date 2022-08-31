be70d397aaab3a1c0d74c45e846bf8df0466398d
Cherrue <th885172@gmail.com>
2022-08-31 23:44:54 +0900

    [Fix] 🔀 Member entity 변경에 따른 수정사항 Room 쪽에 반영

--- src/main/java/com/autorush/rushchat/member/entity/Member.java
+++ src/main/java/com/autorush/rushchat/member/entity/Member.java
@@ -1,20 +1,16 @@
 package com.autorush.rushchat.member.entity;
 
-import com.autorush.rushchat.domain.BaseTimeEntity;
+import com.autorush.rushchat.common.BaseTimeEntity;
 import com.autorush.rushchat.member.type.Role;
+import java.util.Date;
 import java.util.Objects;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GenerationType;
-import javax.persistence.IdClass;
-import javax.persistence.Table;
-import lombok.Builder;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Date;
+import javax.persistence.IdClass;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
